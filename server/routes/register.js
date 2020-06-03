7fa50658e0b480508474d845ed2847c55cc7781e
Jinho Choi <choijinho817@gmail.com>
2020-06-03 20:30:06 +0900

    fix error of register.js, start making find.js

--- server/routes/register.js
+++ server/routes/register.js
@@ -10,10 +10,8 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next){
-  console.log("entered post router")
     const username = req.body.name;
     const studentId = req.body.studentId;
-    console.log("request name, studentId is ", username, studentId)
     //req로 name과 studentId가 왔을 때, database의 자료와 비교하여 없는 자료면 다시하라고 하며, 있으면 username을 설정하여 메인페이지로 redirect
      fs.readFile('./data/Register.json', async (err, data)=>{
       if(err){
@@ -21,19 +19,20 @@ router.post('/', function(req, res, next){
         return;
       }
       let usersdata = JSON.parse(data);
-      console.log("parsed data is ", usersdata);
+      let res_status = 404;
       usersdata.forEach(async (userdata) => {
-        //console.log(`${userdata.name ===username}, ${userdata.studentId===studentId}`);
+        console.log(`${userdata.name ===username}, ${userdata.studentId===studentId}`);
          if (userdata.name === username && userdata.studentId === studentId){
            //register success
-           console.log(`Register Success!! Hello ${username}(${studentId}), welcome to KAISPORTS!`);
+           res_status=200;
+           res.status(res_status);
            res.redirect(`/?name=${username}&studentId=${studentId}`);
          }
          else{
            console.error("cannot find any userdata, please check the name and studentId");
          }
       })
-      res.redirect('/register');
+      if(res_status !==200){res.redirect('/register');}
     });
 })
 
